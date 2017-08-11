098b7743cd6dbc630e5dd52ef15bc1ff2b78e1e9
Viren Sareen <viren.sareen@asos.com>
2017-08-11 13:49:54 +0100

    Bubble Sort added

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.ExceptionServices;
+
+namespace BubbleSort
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var yourarray = new int[] {1, 4, 0, 2};
+            //Insert your values in the array above
+            BubbleSort(yourarray);
+        }
+
+        public static int[] BubbleSort(int[] input)
+        {
+            var temp = 0;
+
+            for (var write = 0; write < input.Length; write++)
+                {
+                    for (var sorter = 0; sorter < input.Length - 1; sorter++)
+                        {
+                            if (input[sorter] > input[sorter + 1])
+                            {
+                                temp = input[sorter + 1];
+                                input[sorter + 1] = input[sorter];
+                                input[sorter] = temp;
+                            }
+                        }
+                }
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                Console.Write(input[i] + ", ");
+            }
+
+            Console.ReadLine();
+            return input;
+        }
+
+    }
+}
